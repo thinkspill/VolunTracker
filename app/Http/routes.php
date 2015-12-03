d18f8f16146964dcb970c95e674b08f13170a925
Ian Cook <git@thinkspill.org>
2015-12-03 15:18:02 -0800

    adds test route and option to disable debug bar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,8 +7,10 @@ Route::get('/discover', 'Home\DiscoverController@discoverFamilyUnits');
 Route::get('/sync', 'Home\SyncController@syncSurveyMonkey');
 Route::get('/pdf', 'Home\ReportController@pdf');
 Route::get('/report', 'Home\ReportController@index');
+Route::get('/report_test', 'Home\ReportController@test');
 Route::get('/dev', 'Home\DevController@dev');
 Route::get('/family/{family_id}', 'Home\FamilyController@show');
+//\Debugbar::disable();
 
 //\DB::listen(function($sql, $bindings, $time) {
 //    r($sql);
