4b1111ce12c8f8c78d3155dc822e1784c001101b
Ian Cook <git@thinkspill.org>
2015-12-02 21:58:45 -0800

    condensed proofing view

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,65 +1,103 @@
 @extends('layout')
 @section('content')
+    <style>
+        .container {
+            max-width: 100%;
+        }
+    </style>
     <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-12">
             <h3>Meets/exceeds</h3>
             <table class="table table-condensed" id="meets">
-                @foreach ($exceeds + $meets as $f)
+                @foreach (array_merge($exceeds, $meets) as $f)
                     <tr>
                         <td>
-                            <ul>
-                                @foreach ($f['guardians'] as $g)
-                                    <li>
-                                        <b>Guardian</b>: {{$g['first']}} {{$g['last']}}
-                                    </li>
-                                @endforeach
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <ul>
+                                        <li>
+                                            <b>Guardians</b>:
+                                            @foreach ($f['guardians'] as $g)
+                                                [{{$g['first']}} {{$g['last']}}]
+                                            @endforeach
+                                        </li>
 
-                                @foreach ($f['students'] as $g)
-                                    <li>
-                                        <b>Student</b>: {{$g['first']}} {{$g['last']}}
-                                    </li>
-                                @endforeach
+                                        <li>
+                                            <b>Students</b>:
+                                            @foreach ($f['students'] as $g)
+                                                [{{$g['first']}} {{$g['last']}}]
+                                            @endforeach
+                                        </li>
 
-                                <li><b>Hours</b>: {{$f['hours']}} ( {{$f['ratio']}} )</li>
+                                        <li><b>Hours</b>: {{$f['hours']}} ( {{$f['ratio']}}% )</li>
 
-                                {{--[ {{$f['family_hash']}} ]--}}
-                            </ul>
+                                        {{--[ {{$f['family_hash']}} ]--}}
+                                    </ul>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ul>
+                                        @foreach ($f['log'] as $log)
+                                            <li>{{$log['date']}}: <b>{{$log['hours']}}</b></li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+
+
                         </td>
                     </tr>
                 @endforeach
             </table>
         </div>
-        <div class="col-sm-4">
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
             <h3>Under</h3>
             <table class="table table-condensed" id="under">
                 @foreach ($under as $f)
                     <tr>
                         <td>
-                            <ul>
-                                @foreach ($f['guardians'] as $g)
-                                    <li>
-                                        <b>Guardian</b>: {{$g['first']}} {{$g['last']}}
-                                    </li>
-                                @endforeach
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <ul>
+                                        <li>
+                                            <b>Guardians</b>:
+                                            @foreach ($f['guardians'] as $g)
+                                                [{{$g['first']}} {{$g['last']}}]
+                                            @endforeach
+                                        </li>
 
-                                @foreach ($f['students'] as $g)
-                                    <li>
-                                        <b>Student</b>: {{$g['first']}} {{$g['last']}}
-                                    </li>
-                                @endforeach
+                                        <li>
+                                            <b>Students</b>:
+                                            @foreach ($f['students'] as $g)
+                                                [{{$g['first']}} {{$g['last']}}]
+                                            @endforeach
+                                        </li>
 
-                                <li><b>Hours</b>: {{$f['hours']}} ( {{$f['ratio']}} )</li>
+                                        <li><b>Hours</b>: {{$f['hours']}} ( {{$f['ratio']}}% )</li>
 
-                                {{--[ {{$f['family_hash']}} ]--}}
-                            </ul>
+                                        {{--[ {{$f['family_hash']}} ]--}}
+                                    </ul>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ul>
+                                        @foreach ($f['log'] as $log)
+                                            <li>{{$log['date']}}: <b>{{$log['hours']}}</b></li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+
+
                         </td>
                     </tr>
                 @endforeach
             </table>
 
         </div>
-
-        <div class="col-sm-4">
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
             <h3>None</h3>
 
             <table class="table table-condensed" id="none">
@@ -79,7 +117,7 @@
                                     </li>
                                 @endforeach
 
-                                <li><b>Hours</b>: {{$f['hours']}} ( {{$f['ratio']}} )</li>
+                                <li><b>Hours</b>: {{$f['hours']}} ( {{$f['ratio']}}% )</li>
 
                                 {{--[ {{$f['family_hash']}} ]--}}
                             </ul>
@@ -87,14 +125,10 @@
                     </tr>
                 @endforeach
             </table>
-
         </div>
     </div>
 
 @endsection
 
 @section('footer_scripts')
-    <script>
-        $('#meets').DataTable();
-    </script>
 @endsection
